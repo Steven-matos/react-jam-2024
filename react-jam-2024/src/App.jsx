cfe4d2141678c194725ec0d655600db39c8ab402
MV5174 <ichigo509@yahoo.com>
2024-10-02 13:39:31 -0400

    added Sprite component, added background with sprite component

--- react-jam-2024/src/App.jsx
+++ react-jam-2024/src/App.jsx
@@ -1,18 +1,23 @@
-//import { useEffect } from 'react';
 import { useEffect } from 'react';
 import Canvas from './components/Canvas.jsx'
-import Sprite from './components/Sprite';
+import Player from './components/Player.jsx';
+import Sprite from './components/Sprite.jsx';
+import level1 from './assets/level/Starter_Level.png'
 
 function App() {
 
   useEffect(() => {
-    
+
   })
 
   return (
     <div>
+      {/* <div style={{ position: 'absolute', zIndex: '1' }}>
+        <canvas ref={canvasRef} />
+      </div> */}
       <Canvas style={{ position: 'absolute', zIndex: '-1' }} />
-      <Sprite position={{x:150, y:50}} style={{ position: 'absolute', zIndex: '1' }} />
+      <Sprite imageSrc={level1} position={{ x: 0, y: 0 }} style={{ position: 'absolute', zIndex: '1' }} />
+      <Player position={{ x: 100, y: 50 }} style={{ position: 'absolute', zIndex: '2' }} />
     </div>
   )
 }
---
+++ react-jam-2024/src/components/Player.jsx
@@ -0,0 +1,173 @@
+/* eslint-disable react/prop-types */
+import { useRef, useEffect } from 'react';
+
+export default function Player(
+    {
+        position = {
+            x: 0,
+            y: 0
+        },
+        width = 32,
+        height = 32,
+        color = 'red',
+        style = {
+            position: 'absolute',
+            zIndex: '1'
+        },
+        sides = {
+            bottom: position.y + 100,
+            top: 100,
+            left: 100,
+            right: 100
+        },
+        velocity = {
+            x: 0,
+            y: 0
+        },
+        gravity = .03,
+        preventInput = false,
+        direction = 'right',
+        jumps = 0,
+        jumpCount = 1
+    }
+) {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        // Set canvas dimensions
+        canvas.width = 1024;
+        canvas.height = 576;
+
+        const keys = {
+            w: {
+                pressed: false
+            },
+            a: {
+                pressed: false
+            },
+            d: {
+                pressed: false
+            },
+
+        }
+
+        function handleInput() {
+            window.addEventListener('keydown', (event) => {
+                if (preventInput) {
+                    return
+                }
+                switch (event.key) {
+                    case 'w':
+                        if (jumps < jumpCount) {
+                            velocity.y = -2.0;
+                            jumps++;
+                        }
+                        break
+                    case 'a':
+                        keys.a.pressed = true;
+                        break
+                    case 'd':
+                        keys.d.pressed = true;
+                        break
+                    // case 'f':
+                    //     for (let i = 0; i < doors.length; i++) {
+                    //         const door = doors[i];
+
+                    //         if (player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width &&
+                    //             player.hitbox.position.x >= door.position.x &&
+                    //             player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+                    //             player.hitbox.position.y <= door.position.y + door.height &&
+                    //             !door.locked
+                    //         ) {
+                    //             player.velocity.x = 0;
+                    //             player.velocity.y = 0;
+                    //             player.preventInput = true;
+                    //             player.switchSprite('enterDoor');
+                    //             door.play();
+                    //             return
+                    //         }
+                    //     }
+                    //     break
+                }
+            })
+
+            window.addEventListener('keyup', (event) => {
+                switch (event.key) {
+                    case 'a':
+                        keys.a.pressed = false;
+                        break
+                    case 'd':
+                        keys.d.pressed = false;
+                        break
+                }
+            })
+
+            if (keys.d.pressed && keys.a.pressed) {
+                velocity.x = 0;
+                // if (direction === 'right') {
+                //     switchSprite('idleRight');
+                // } else {
+                //     switchSprite('idleLeft');
+                // } 
+            } else if (keys.a.pressed) {
+                //switchSprite('runLeft');
+                velocity.x = -1.0;
+                //direction = 'left';
+            } else if (keys.d.pressed) {
+                //switchSprite('runRight');
+                velocity.x = 1.0;
+                //direction = 'right';
+            }
+            //else if (direction === 'right' && velocity.x == 0) {
+            //switchSprite('idleRight');
+            //} else {
+            //switchSprite('idleLeft');
+            //}
+        }
+
+        function animate() {
+            window.requestAnimationFrame(animate);
+            velocity.x = 0;
+            
+            render();
+            handleInput(keys);
+            update();
+        }
+
+        function render() {
+            // Clear the canvas before redrawing
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draw the sprite (a 100x100 box)
+            context.fillStyle = color;
+            context.fillRect(position.x, position.y, width, height);
+        }
+
+        function update() {
+            position.x += velocity.x;
+            position.y += velocity.y;
+            sides.bottom = position.y + height;
+
+            //Above bottom of canvas
+            if (sides.bottom + velocity.y < canvas.height) {
+                velocity.y += gravity;
+            } else {
+                velocity.y = 0;
+                jumps = 0;
+            }
+        }
+
+        animate();
+
+
+    }, [position, width, height, color, style, sides, velocity, gravity, preventInput, direction, jumps, jumpCount]); // Re-render when props change
+
+    return (
+        <div style={style}>
+            <canvas ref={canvasRef} />
+        </div>
+    );
+};
