08ac760c97e40fca4abb49f592a0fb4c6c38c513
MV5174 <ichigo509@yahoo.com>
2024-10-05 19:27:46 -0400

    adding level switching for png

---
+++ react-jam-2024/src/data/levels.jsx
@@ -0,0 +1,12 @@
+const levels = {
+    1: {
+        src: '/src/assets/level/Starter_Level.png',
+        name: 'Level 1'
+    },
+    2: {
+        src: '/src/assets/level/Level_2.png',
+        name: 'Level 2'
+    }
+}
+
+export default levels
